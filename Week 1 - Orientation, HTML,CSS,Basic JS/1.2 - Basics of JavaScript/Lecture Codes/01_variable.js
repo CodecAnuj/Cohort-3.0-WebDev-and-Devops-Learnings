d5f4e5e7293fc6f891b2c9312cd815b029a1999b
Anuj Tiwari <anujtiwari9111@gmail.com>
2025-09-25 21:57:47 +0530

    1.2 - Basics of JavaScript lecture code added

---
+++ Week 1 - Orientation, HTML,CSS,Basic JS/1.2 - Basics of JavaScript/Lecture Codes/01_variable.js
@@ -0,0 +1,19 @@
+let firstName = "Anuj";
+console.log(firstName); // Anuj
+
+const age = 21;
+console.log(age); // 21
+
+var isStudent = true;
+console.log(isStudent); // true
+
+
+console.log("firstName"); // firstName
+console.log("Anuj"); // Anuj
+
+
+isStudent = "Deepak";
+console.log(isStudent); // Deepak
+
+isStudent = 10;
+console.log(isStudent); // 10
---
+++ Week 1 - Orientation, HTML,CSS,Basic JS/1.2 - Basics of JavaScript/Lecture Codes/03_datatypes.js
@@ -0,0 +1,29 @@
+let number = 44; // Number
+let string = "Hello World!"; // String
+let isActive = false; // Boolean
+let numbers = [1, 2, 3]; // Array
+
+console.log(number); // 44
+console.log(string); // Hello World!
+console.log(isActive); // false
+console.log(numbers); // [1, 2, 3]
+
+
+let user1 = "Harkirat";
+let user2 = "Anuj";
+let user3 = "Deepak";
+
+console.log(user1); // Harkirat
+console.log(user2); // Anuj
+console.log(user3); // Deepak
+
+
+// Array
+let users = ["Harkirat", "Anuj", "Deepak"];
+
+console.log(users); // ["Harkirat", "Anuj", "Deepak"]
+
+console.log(users[0]); // Harkirat
+console.log(users[1]); // Anuj
+console.log(users[2]); // Deepak
+console.log(users[3]); // undefined
